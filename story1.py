ca10340c15e3f10c995638198d4f584eeb911110
ctinaa <christina.fcook@yahoo.com>
2015-11-16 00:34:57 -0800

    :fries: Story 1 completed; Simple mad lib sentence

---
+++ story1.py
@@ -0,0 +1,11 @@
+#extremely basic python program
+
+def madlib_1() : 
+	print "Welcome to the first crazy Mad Lib"
+
+	food = raw_input("Enter a type of food: ")
+	verb = raw_input("Enter a type of verb: ")
+	noun = raw_input("Enter a type of noun: ")
+
+
+	print "My mom's", food, "was", verb, "away since she told me to eat all my", noun 
